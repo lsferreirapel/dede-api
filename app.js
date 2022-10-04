b38f8fdc9e7348c09e90a58d247a2fae8568ce9b
Lucas Ferreira <lsferreirapel@gmail.com>
2022-10-04 17:22:20 -0300

    chore: enable cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./src/routes.js";
+import cors from 'cors'
 
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(routes);
 
 app.get("/", (req, res) => {
@@ -11,5 +13,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
